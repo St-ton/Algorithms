4bd7ac7727c755b0022dbc9ec9edfdad51140bbe
Stanislav Tonkich <st.tonkikh@gmail.com>
2023-04-19 15:16:51 +0200

    add hw1804

---
+++ HomeWork/1804/script.js
@@ -0,0 +1,176 @@
+// (Выполнить обобщающий тест по курсу алгоритмов)
+// [https://forms.gle/qfT3XesbDkhMMCsi8]
+// Подготовить вопросы по материалам курса к преподавателю
+
+Asymptotic analysis is about ...?
+
++ Space Complexity
+  World Complexity
++ Time Complexity
+  Galaxy Complexity
+
+Which algorithm is faster?
+
+  O(1000 n log n)
+  O(10 n log n)
+  O(2 log n 1000)
++ O(n)
+
+What base case will work if n >= 30 && n < 100?
+
+  int f(int n) if (n <= 1) return 1; else return n*f(n - 1);
++ int f(int n) if (n == 100) return 1; else return n*f(n - 1);
+  int f(int n) if (n == -1) return 1; else return n*f(n + 1);
+
+
+Select the algorithm analysis that evaluates an algorithm in its best case.
+
+  Big O notation
++ Big Omega notation
+  Big Theta notation
+
+There is only one possible algorithm for a specific task.
+
+  true
++ false
+
+If you search linearly in the array of 'Ar' for a value value, what will be the worst case value ? 
+Ar = | 3 | 4| 1| 9| 6| 2| 7|
+  2
+  3
+  9
++ 7
+
+O(n ^ 2) 
+
+  Merge sort
++ Bubble sort
+  Linear search
+  Quick sort
+  Binary search
+
+
+Which of the following data structures is NOT a linear data structure?
+
+  Arrays
+  Linked lists
++ Trees
+  Stack
+
+Which of the following data structures is a linear data structure?
+
+  Arrays
+  AVL tree
++ Queue
+  Map
+
+The function that finds the location of the element with a given value is:
+
+  Traversal
++ Search
+  Sort
+
+In a red-black tree, a black node must have only red children.
+
+  true
++ false
+
+
+Dynamic programming optimizes direct recursion.
+
++ true
+  false
+
+Which of the following is true for a red - black tree ?
+
+  The root is red
++ Root and leaves are black.
+  All trees are green, there is no right answer.
+  Every node is either red or black.
+  Every leaf (NULL) is black.
+
+Stack is...
+
+  First in, first out
+  First in, last out
+  Last in, last out
++ Last in, first out
+
+
+How many iterations will be required to sort the following array using a bubble sort algorithm?
+
+Ar = |4|5|7|6| 
+  2
+  3
++ 4
+  5
+  6
+
+QUEUE does NOT support the following operation.
+
+  Insertion
+  Deletion
++ Recovery
+  Traversal
+
+The allowed values for height of an AVL tree are...
+
+  -2, 0, 2
+  0
+  0, 1
++ -1, 0, 1
+
+Which of the following is a double rotation?
+
+  LL
+  LR
+  RR
+  + LRL
+
+
+The sequence of insertion 1, 2, 4 will lead to imbalance. Which rotation can be used to balance the tree?
+
+  RR rotation
++ LL rotation
+  LR rotation
+  RL rotation
+
+What is the time complexity of counting the number of elements in a linked list?
+
++ O(n)
+  O(n log n)
+  O(1)
+  O(log n)
+
+The LinkedList class implements from...
+
+  Interface Array
+  Interface Object
+  Interface Map
++ Interface List
+  
+  
+Given the following method, what will the test return if
+a = {0, 2, 3, 4} and v = 1?
+
+
+You can only use a binary search algorithm if the data is ordered. i.e. smallest to largest.
+
++ true
+  false
+
+In computer science, bubble sort is an efficient algorithm that implements a “divide and conquer” approach.
+
+  true
++ false
+
+In computer science, linear search is an efficient algorithm that implements a “divide and conquer” approach.
+
+true
+false
+
+
+// Продолжаем писать код.
+// презентация https://docs.google.com/presentation/d/1B7cPz4vCayMk1THoApwB3f56WMcQ2u/edit?usp=sharing&ouid=112472413658880190772&rtpof=true&sd=true
+
+К сожалению презентация не доступна
